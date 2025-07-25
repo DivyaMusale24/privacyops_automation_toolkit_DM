3c75dd0d1cab8a8c8bc2966cad56f8223dc05cc0
DivyaMusale24 <168490372+DivyaMusale24@users.noreply.github.com>
2025-07-24 20:55:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from app.data.storage import init_db
 
 def main():
     st.set_page_config(page_title="PrivacyOps Automation Toolkit", layout="wide")
-    st.title("yPrivacyOps Automation Toolkit")
+    st.title("PrivacyOps Automation Toolkit by DM")
     st.markdown("Automate vendor risk, RoPA & PIA tracking, and privacy reporting.")
     init_db()
     show_dashboard()
